4efccae8fe04cac4fb5102bf65e02f60ad9d36e3
BreenF97 <brendaferessin@gmail.com>
2024-11-22 09:32:59 -0300

    Update Feressin_Brenda-Lista_EDC.asm

--- Feressin_Brenda-Lista_EDC.asm
+++ Feressin_Brenda-Lista_EDC.asm
@@ -1,5 +1,109 @@
 		###CODIGO DEL PDF, PARA IR AGREGANDO
 
+	.data
+slist:	.word 0		#puntero - lo utilizan las funciones smalloc y sfree
+cclist:	.word 0		#puntero a la lista de categorias - circular category list
+wclist:	.word 0		#puntero a la categoria seleccionada en curso - working category list
+schedv:	.space 32	#vector de direcc scheduler vector- 32 bytes para las 8 opciones del menu
+menu:	.ascii "Colecciones de objetos categorizados\n"
+	.ascii "====================================\n"
+	.ascii "1-Nueva categoria\n"
+	.ascii "2-Siguiente categoria\n"
+	.ascii "3-Categoria anterior\n"
+	.ascii "4-Listar categorias\n"
+	.ascii "5-Borrar categoria actual\n"
+	.ascii "6-Anexar objeto a la categoria actual\n"
+	.ascii "7-Listar objetos de la categoria\n"
+	.ascii "8-Borrar objeto de la categoria\n"
+	.ascii "0-Salir\n"
+	.asciiz "Ingrese la opcion deseada: "
+	
+error:	.asciiz "Error: "
+return:	.asciiz "\n"
+catName:.asciiz "\nIngrese el nombre de una categoria: "
+selCat: .asciiz "\nSe ha seleccionado la categoria: "
+idObj:	.asciiz "\nIngrese el ID del objeto a eliminar: "
+objName:.asciiz "\nIngrese el nombre de un objeto: "
+success:.asciiz "La operacion se realizo con exito\n\n"	
+
+.text
+
+main:	la $t0, schedv		#inicializacion
+#	la $t1, newcategory	#cargo c/ direcc del menu, y la guardo en el space schedv
+#	sw $t1, 0($t0)
+#	la $t1, nextcategory
+#	sw $t1, 4($t0)
+#	la $t1, prevcategory
+#	sw $t1, 8($t0)
+#	la $t1, listcategory
+#	sw $t1, 12($t0)
+#	la $t1, delcategory
+#	sw $t1, 16($t0)
+#	la $t1, newobject
+#	sw $t1, 20($t0)
+#	la $t1, listobjects
+#	sw $t1, 24($t0)
+#	la $t1, delobject
+#	sw $t1, 28($t0)
+	
+	
+	
+#Imprimo menu
+	li $v0, 4
+	la $a0, menu
+	syscall
+	
+	
+#Tomo el input del usuario	
+	li $v0, 5
+	syscall
+	move $t1, $v0	#Lo paso a t1 porque al v0 lo voy a estar pisando constantemente con syscall seguro
+	
+	
+	
+	
+	
+#smalloc:
+#	lw $t0, slist		
+#	beqz $t0, sbrk
+#	move $v0, $t0
+#	lw $t0, 12($t0)
+#	sw $t0, slist
+#	jr $ra
+	
+#sbrk: 
+#	li $a0, 16 	#node size fixed 4 word
+#	li $v0, 9
+#	syscall		#return node address in v0
+#	jr $ra
+	
+#sfree: 	
+#	lw $t0, slist
+#	sw $t0, 12($a0)
+#	sw $a0, slist 	#a0 node adrdress in unused list
+#	jr $ra
+	
+	
+#newcategory:
+#	addiu $sp, $sp, -4
+#	sw $ra, 4($sp)
+#	la $a0, catName	#input category name
+#	jal getblock
+#	move $a2, $v0 	#a2= *char to category name
+#	la $a0, cclist	#a0=list
+#	li $a1, 0 	#a1=null
+#	jal addnode
+#	lw $t0, wclist
+#	bnez $t0, newcategory_end
+#	sw $v0, wclist 	#update working list if was NULL
+	
+#newcategory_end: 
+#	li $v0, 0 	#return success
+#	lw $ra, 4($sp)
+#	addiu $sp, $sp, 4
+#	jr $ra
+	
+
 
 
 
